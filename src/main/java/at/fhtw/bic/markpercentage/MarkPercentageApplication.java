568bf99e9592767e6579a8856973c4d026374d82
Nermina Salkic <ic21b114@technikum-wien.at>
2022-12-21 18:04:35 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/markpercentage/MarkPercentageApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.markpercentage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkPercentageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkPercentageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/markpercentage/MarkPercentageApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.markpercentage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkPercentageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
