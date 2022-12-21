3888485b7241fe574a27d8b2d16bc562d28bdd2c
Nermina Salkic <ic21b114@technikum-wien.at>
2022-12-21 18:43:56 +0100

    neuer code für percentage berechnung

--- src/main/java/at/fhtw/bic/markpercentage/Percentage/markPercentage.java
+++ src/main/java/at/fhtw/bic/markpercentage/Percentage/markPercentage.java
@@ -1,16 +1,27 @@
 package at.fhtw.bic.markpercentage.Percentage;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class markPercentage {
     @GetMapping("/markPercentage")
 
-    public String getMark(int percentage) {
+    public String getMark(@RequestParam int percentage) {   //Das wird vom Client eingegeben: api/mark?percentage=90 → 1
         int mark;
-
-
-        return "";
+        if (percentage >= 88) {
+            return "1";
+        } else if (percentage >= 75) {
+            return "2";
+        } else if (percentage >= 63) {
+            return "3";
+        } else if (percentage >= 50) {
+            return "4";
+        } else if (percentage < 50) {
+            return "5";
+        } else {
+            return "No valid percentage. Please enter again.";
+        }
     }
 }
