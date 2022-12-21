a6d204d6abaefaf8778dfdb9157f4876d1f3ebdb
Nermina Salkic <ic21b114@technikum-wien.at>
2022-12-21 18:13:47 +0100

    Initial commit

--- src/main/java/at/fhtw/bic/markpercentage/MarkPercentageApplication.java
+++ src/main/java/at/fhtw/bic/markpercentage/MarkPercentageApplication.java
@@ -2,11 +2,12 @@ package at.fhtw.bic.markpercentage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class MarkPercentageApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MarkPercentageApplication.class, args);
     }
 
